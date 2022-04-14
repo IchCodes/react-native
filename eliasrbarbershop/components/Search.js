b5696c8d8e844fcf5e7a85c94fc316ac71589ad3
Hichame Sraidi <hichame.sraidi@gmail.com>
2022-04-14 03:51:29 +0200

    Ajout des styles et FlatList

--- eliasrbarbershop/components/Search.js
+++ eliasrbarbershop/components/Search.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { View, Button, TextInput} from 'react-native';
+import { StyleSheet, View, Button, FlatList, Text, TextInput } from 'react-native';
+import films from '../helpers/filmData';
 
 class Search extends React.Component {
     render(){
         return(
-            <View>
-                <TextInput placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={()=>{}}/>     
-            </View>
+            <View style={styles.main_container}>
+                 <TextInput placeholder='Titre du film' style={styles.textinput} />
+                 <Button title='Rechercher' onPress={() => { }} />
+                 <FlatList
+                     data={films}
+                     keyExtractor={(item) => item.id.toString()}
+                     renderItem={({ item }) => <Text>{item.title}</Text>}
+                 />
+                 </View>
         )
     }
 
 }
+const styles = StyleSheet.create({
+    main_container: {
+        marginTop: 40,
+        flex: 1
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    }
+})
 
- export default Search
+export default Search 
