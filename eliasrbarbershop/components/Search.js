5747b1d6f6882869b92b8f275f794e7c441a8869
Hichame Sraidi <hichame.sraidi@gmail.com>
2022-04-14 05:58:34 +0200

    Implementation écran chargement

--- eliasrbarbershop/components/Search.js
+++ eliasrbarbershop/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Button, FlatList, Text, TextInput } from 'react-native';
+import { StyleSheet, View, Button, FlatList, Text, TextInput, ActivityIndicator } from 'react-native';
 import films from '../helpers/filmData';
 import FilmItem from './FilmItem';
 import { getFilmsFromApiWithSearchedText } from '../API/TMDB'
@@ -10,7 +10,8 @@ class Search extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            films: []
+            films: [],
+            isLoading: false
         }
         this.searchedText = ""
     }
@@ -19,12 +20,28 @@ class Search extends React.Component {
         this.searchedText = text
     }
     _loadFilms() {
+        this.setState({ isLoading: true })
         if(this.searchedText.length >0){
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => this.setState({ films: data.results }));
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data => 
+                this.setState({ 
+                    films: data.results,
+                    isLoading: false
+                }));
         }
     }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+          return (
+            <View style={styles.loading_container}>
+              <ActivityIndicator size='large' />
+              {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+            </View>
+          )
+        }
+      }
     render() {
-        console.log("RENDER");
+        console.log(this.state.isLoading);
         return (
             <View style={styles.main_container}>
                 <TextInput
@@ -42,6 +59,7 @@ class Search extends React.Component {
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => <FilmItem film={item} />}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -59,7 +77,16 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
-    }
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
 })
 
 export default Search 
