923be66628f59ac5d8d64574b493f799e3d69a1b
Hichame Sraidi <hichame.sraidi@gmail.com>
2022-04-18 08:06:08 +0200

    Implementation Bottom Tab

--- eliasrbarbershop/Navigation/Navigation.js
+++ eliasrbarbershop/Navigation/Navigation.js
@@ -1,9 +1,14 @@
+import React from 'react'
+import { StyleSheet ,Image } from 'react-native'
 import { 
     createStackNavigator 
 } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Search from '../components/Search'
 import FilmDetail from '../components/FilmDetails'
+import Favorites from '../components/Favorites'
 
 const SearchStackNavigation = createStackNavigator({
     Search: {
@@ -20,4 +25,41 @@ const SearchStackNavigation = createStackNavigator({
     }
 }) 
 
-export default createAppContainer(SearchStackNavigation)
+const MoviesTabNavigator = createBottomTabNavigator({
+    Search: {
+        screen: SearchStackNavigation,
+        navigationOptions: {
+            tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+              return <Image
+                source={require('../Images/ic_search.png')}
+                style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+            }
+          }
+    },
+    Favorites: {
+        screen: Favorites,
+        navigationOptions: {
+            tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+              return <Image
+                source={require('../Images/ic_favorite.png')}
+                style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+            }
+          }
+    }
+}, {
+    tabBarOptions: {
+        showLabel: false,
+        showIcon: true,
+        activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+        inactiveBackgroundColor: '#FFFFFF' // Couleur d'arrière-plan des onglets non sélectionnés
+    }
+})
+
+const styles = StyleSheet.create({
+    icon: {
+      width: 30,
+      height: 30
+    }
+  })
+
+export default createAppContainer(MoviesTabNavigator)
